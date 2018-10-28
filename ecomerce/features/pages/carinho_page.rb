c68d1e1dfc541d22d71aa600a9b4cc82e477ce6b
bruno batista <batista@brunos-MacBook-Pro-2.local>
2018-10-28 20:38:24 -0300

    segunda aula

--- ecomerce/features/pages/carinho_page.rb
+++ ecomerce/features/pages/carinho_page.rb
@@ -1,10 +1,4 @@
 class Carrinho < SitePrism::Page
+  # Onde  a classe "".carrinho-direita" é o pai dos elementos "ul" e assim em diante são os filhos
   element :ultimo_radio, '.carrinho-direita > ul:nth-child(2) > li:nth-child(3) > label > input'
 end
-
-# mapeando elementos com 
-
-# onde .carrinho-direita e o pai dos elementos o ul en diante sao os filhos
-
-
-
